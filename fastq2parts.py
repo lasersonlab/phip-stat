a48146519faf4e566b34c6ec41e3973fac4a5ebd
Uri Laserson <uri.laserson@gmail.com>
2011-10-02 22:19:29 -0400

    Stupid bug

--- fastq2parts.py
+++ fastq2parts.py
@@ -18,7 +18,7 @@ packetsize = args.packetsize
 
 num_processed = 0
 file_num = 1
-outfilename = os.path.join(work_dir,parts_dir,'part.%s.fastq' % file_num)
+outfilename = os.path.join(output_dir,'part.%s.fastq' % file_num)
 for record in SeqIO.parse(input_filename,'fastq'):
     if num_processed == 0:
         op = open(outfilename,'w')
@@ -30,7 +30,7 @@ for record in SeqIO.parse(input_filename,'fastq'):
         op.close()
         num_processed = 0
         file_num += 1
-        outfilename = os.path.join(work_dir,parts_dir,'part.%s.fastq' % file_num)
+        outfilename = os.path.join(output_dir,'part.%s.fastq' % file_num)
 
 if not op.closed:
     op.close()
