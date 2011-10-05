f0eb8b15cce3af7f20c84cbdd16d074910a1df83
Uri Laserson <uri.laserson@gmail.com>
2011-10-04 20:22:17 -0400

    Modified parts2barcodes to accomodate diff header
    Also allow one mismatch in the barcode.

--- parts2barcodes.py
+++ parts2barcodes.py
@@ -5,6 +5,16 @@ import re
 
 bcre = re.compile(r'#(.*)/')
 
+def hamming1(s):
+    s = s.upper()
+    alts = {'A':'CGTN','C':'AGTN','G':'ACTN','T':'ACGN'}
+    mutants = []
+    for i in range(len(s)):
+        for alt in alts[s[i]]:
+            mutant = s[:i] + alt + s[i+1:]
+            mutants.append(mutant)
+    return mutants
+
 argparser = argparse.ArgumentParser(description=None)
 argparser.add_argument('-i','--input',required=True)
 argparser.add_argument('-o','--output',required=True)
@@ -22,15 +32,20 @@ outhandles = {}
 with open(mapping_file,'r') as ip:
     for line in ip:
         data = line.split()
-        barcode2sample[data[0]] = data[1]
-        outhandles[data[1]] = open(os.path.join(output_dir,data[1]+'.aln'),'w')
+        bc = data[0]
+        sample = data[1]
+        barcode2sample[bc] = sample
+        for mut in hamming1(bc):
+            barcode2sample[mut] = sample
+        outhandles[sample] = open(os.path.join(output_dir,data[1]+'.aln'),'w')
 
 # iterate through alignments
 for infilename in glob.glob(os.path.join(input_dir,'*.aln')):
     with open(infilename,'r') as ip:
         for line in ip:
-            read_name = line.split()[0]
-            bc = bcre.search(read_name).group(1)
+            # read_name = line.split()[0]
+            # bc = bcre.search(read_name).group(1)
+            bc = line.split()[1].split(':')[-1]
             try:
                 sample = barcode2sample[bc]
             except KeyError:
